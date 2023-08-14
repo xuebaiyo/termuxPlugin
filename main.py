500ee42c7fe6c49b1b587f41e748276212d5b10d
xuebai <53637517+xuebaiyo@users.noreply.github.com>
2023-08-14 22:52:31 +0800

    Update main.py

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ class SysStatPlugin(Plugin):
             cpu_freq = psutil.cpu_freq()
 
             #获取用户输入的指令
-            ossh = os.popen('ping 192.168.191.2').readlines()
+            #ossh = os.popen('ping 192.168.191.2').readlines()
             #print(ossh)
             
             res = f"""====执行结果====
@@ -46,11 +46,19 @@ CPU使用率: {psutil.cpu_percent(interval=1):.2f}%
     @on(PersonCommandSent)
   def person_normal_message_received(self, event: EventContext, **kwargs):
         msg = kwargs['text_message']
-        ossh = re.search(r"!(.*)", string)  #截取!号后所有字符
-            # 输出测试结果
-            ossh = os.popen('ping 192.168.191.2').readlines()
-            # 回复消息 "hello, <发送者id>!"
+       result = re.search(r"!(.*)", msg)  #截取!号后所有字符
+      if result:
+            matched_text = result.group(1)            # 获取匹配到的结果
+            ossh = os.popen(matched_text).readlines()  #执行指令
             event.add_return("reply", ["命令执行结束!,直接结果为: {ossh}{}!".format(kwargs['sender_id'])])
+
+            # 阻止该事件默认行为（向接口获取回复）
+            event.prevent_default()
+
+else:
+   # print("未找到匹配的内容")
+            # 回复消息 "hello, <发送者id>!"
+            event.add_return("reply", ["请在!t 后面输入指令!,{}!".format(kwargs['sender_id'])])
 
             # 阻止该事件默认行为（向接口获取回复）
             event.prevent_default()
