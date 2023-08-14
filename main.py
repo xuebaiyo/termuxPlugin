92733e3e496a1bb6affe44cb4c078c47e4ac5af6
xuebai <53637517+xuebaiyo@users.noreply.github.com>
2023-08-14 23:47:15 +0800

    Update main.py

--- main.py
+++ main.py
@@ -8,24 +8,28 @@ class HelloPlugin(Plugin):
         pass
 
     @on(PersonNormalMessageReceived)
-    def person_normal_message_received(self, event: EventContext, **kwargs):
-        msg = kwargs['text_message']
-        if "ossh" in msg:  # 如果消息中包含"ossh"字段
-
-            # 输出调试信息
-            logging.debug("ossh command received from {}".format(kwargs['sender_id']))
+def command_send(self, host: PluginHost, event: EventContext, command: str, **kwargs):
+         if command == "ossh":
+             event.prevent_default()
+            event.prevent_postorder()
+            msg = kwargs['text_message']
 
             # 截取"ossh"后面的所有字符
             command = msg.split("ossh", 1)[1].strip()
 
-            # 执行相应的操作，例如调用Linux命令
-            # ...
-
-            # 回复消息
-            event.add_return("reply", ["执行了ossh命令"])
+         try:
+                # 执行Linux指令
+                output = subprocess.check_output(command, shell=True, stderr=subprocess.STDOUT, universal_newlines=True)
+                # 回复执行结果
+             
+                event.add_return("reply", [output])
+            except subprocess.CalledProcessError as e:
+                error_output = e.output if e.output else str(e)
+                
+                # 回复错误信息
+                event.add_return("reply", ["命令执行出错：{}".format(error_output)])
 
             # 阻止该事件默认行为（向接口获取回复）
             event.prevent_default()
-
     def __del__(self):
         pass
