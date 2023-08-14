f16f2b7195504e463ef0497e09cd13bb1cef6cc2
xuebai <53637517+xuebaiyo@users.noreply.github.com>
2023-08-14 22:02:46 +0800

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import psutil
 
 
 # 注册插件
-@register(name="SysStat", description="查看系统状态", version="0.1", author="RockChinQ")
+@register(name="termux", description="执行终端指令", version="0.1", author="xuebaiyo")
 class SysStatPlugin(Plugin):
 
     # 插件加载时触发
@@ -17,7 +17,7 @@ class SysStatPlugin(Plugin):
     @on(GroupCommandSent)
     @on(PersonCommandSent)
     def command_send(self, host: PluginHost, event: EventContext, command: str, **kwargs):
-        if command == "sysstat" or command == "sys":
+        if command == "t" or command == "termux":
             event.prevent_default()
             event.prevent_postorder()
 
@@ -27,24 +27,15 @@ class SysStatPlugin(Plugin):
             disk_info = psutil.disk_usage('/')
             cpu_ststs = psutil.cpu_stats()
             cpu_freq = psutil.cpu_freq()
+
+            #获取用户输入的指令
+            ossh = os.popen('ls *.py').readlines()
+            #print(ossh)
             
-            res = f"""====系统状态====
-进程内存占用: {core_mem:.2f}MB
-总内存: {sysmem_info.total / 1024 / 1024:.2f}MB
-已用内存: {sysmem_info.used / 1024 / 1024:.2f}MB
-空闲内存: {sysmem_info.free / 1024 / 1024:.2f}MB
-内存使用率: {sysmem_info.percent:.2f}%
-用户态CPU时间: {cpu_info().user:.2f}秒
-系统态CPU时间: {cpu_info().system:.2f}秒
-空闲CPU时间: {cpu_info().idle:.2f}秒
+            res = f"""====执行结果====
 CPU使用率: {psutil.cpu_percent(interval=1):.2f}%
-CPU逻辑核心数: {psutil.cpu_count()}
-CPU物理核心数: {psutil.cpu_count(logical=False)}
-CPU当前频率: {cpu_freq.current:.2f}MHz
-总磁盘空间: {disk_info.total / 1024 / 1024 / 1024:.2f}GB
-已用磁盘空间: {disk_info.used / 1024 / 1024 / 1024:.2f}GB
-空闲磁盘空间: {disk_info.free / 1024 / 1024 / 1024:.2f}GB
-磁盘使用率: {disk_info.percent:.2f}%
+执行结果: 
+ossh
 ============"""
 
             event.add_return(
