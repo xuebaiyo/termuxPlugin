be0aa931c29864247e365d8712f2c1273ba2a216
xuebai <53637517+xuebaiyo@users.noreply.github.com>
2023-08-14 23:06:56 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,36 +1,40 @@
 from pkg.plugin.models import *
 from pkg.plugin.host import EventContext, PluginHost
-
-import os
-import psutil
-import re
-
 # 注册插件
-@register(name="termux", description="执行终端指令", version="0.2", author="xuebaiyo")
-class SysStatPlugin(Plugin):
+@register(name="Hello", description="hello world", version="0.1", author="RockChinQ")
+class HelloPlugin(Plugin):
 
     # 插件加载时触发
     # plugin_host (pkg.plugin.host.PluginHost) 提供了与主程序交互的一些方法，详细请查看其源码
     def __init__(self, plugin_host: PluginHost):
         pass
 
-    @on(GroupCommandSent)
-    @on(PersonCommandSent)
-  def person_normal_message_received(self, event: EventContext, **kwargs):
+    # 当收到个人消息时触发
+    @on(PersonNormalMessageReceived)
+    def person_normal_message_received(self, event: EventContext, **kwargs):
         msg = kwargs['text_message']
-       result = re.search(r"!ossh(.*)", msg)  #截取!ossh后所有字符
-      if result:
-            matched_text = result.group(1)            # 获取匹配到的结果
-            ossh = os.popen(matched_text).readlines()  #执行指令
-            event.add_return("reply", ["命令执行结束!,直接结果为: {ossh}{}!".format(kwargs['sender_id'])])
+        if msg == "hello":  # 如果消息为hello
+
+            # 输出调试信息
+            logging.debug("hello, {}".format(kwargs['sender_id']))
+
+            # 回复消息 "hello, <发送者id>!"
+            event.add_return("reply", ["hello, {}!".format(kwargs['sender_id'])])
 
             # 阻止该事件默认行为（向接口获取回复）
             event.prevent_default()
 
-else:
-   # print("未找到匹配的内容")
-            # 回复消息 "hello, <发送者id>!"
-            event.add_return("reply", ["请在!ossh 后面输入指令!,{}!".format(kwargs['sender_id'])])
+    # 当收到群消息时触发
+    @on(GroupNormalMessageReceived)
+    def group_normal_message_received(self, event: EventContext, **kwargs):
+        msg = kwargs['text_message']
+        if msg == "hello":  # 如果消息为hello
+
+            # 输出调试信息
+            logging.debug("hello, {}".format(kwargs['sender_id']))
+
+            # 回复消息 "hello, everyone!"
+            event.add_return("reply", ["hello, everyone!"])
 
             # 阻止该事件默认行为（向接口获取回复）
             event.prevent_default()
