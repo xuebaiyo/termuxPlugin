ce007edfdd213efde190436edd8deb85353d6e44
xuebai <53637517+xuebaiyo@users.noreply.github.com>
2023-08-14 23:18:35 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,44 +1,35 @@
+import re
+import subprocess
 from pkg.plugin.models import *
 from pkg.plugin.host import EventContext, PluginHost
-# 注册插件
-@register(name="Hello", description="hello world", version="0.1", author="RockChinQ")
-class HelloPlugin(Plugin):
 
-    # 插件加载时触发
-    # plugin_host (pkg.plugin.host.PluginHost) 提供了与主程序交互的一些方法，详细请查看其源码
+@register(name="ossh", description="通过qbot执行linux命令", version="0.1", author="xuebaiyo")
+class HelloPlugin(Plugin):
     def __init__(self, plugin_host: PluginHost):
         pass
 
-    # 当收到个人消息时触发
     @on(PersonNormalMessageReceived)
     def person_normal_message_received(self, event: EventContext, **kwargs):
         msg = kwargs['text_message']
-        if msg == "hello":  # 如果消息为hello
+        if "ossh" in msg:  # 判断消息是否包含"ossh"字段
+            # 使用正则表达式截取"ossh"后面的所有字符
+            result = re.search(r"ossh(.*)", msg)
+            if result:
+                command = result.group(1).strip()  # 获取截取到的命令，并去除首尾空格
 
-            # 输出调试信息
-            logging.debug("hello, {}".format(kwargs['sender_id']))
-
-            # 回复消息 "hello, <发送者id>!"
-            event.add_return("reply", ["hello, {}!".format(kwargs['sender_id'])])
-
-            # 阻止该事件默认行为（向接口获取回复）
+                try:
+                    # 执行Linux命令
+                    output = subprocess.check_output(command, shell=True, stderr=subprocess.STDOUT, universal_newlines=True)
+                    # 回复执行结果
+                    event.add_return("reply", [output])
+                except subprocess.CalledProcessError as e:
+                    error_output = e.output if e.output else str(e)
+                    # 回复错误信息
+                    event.add_return("reply", ["命令执行出错：{}".format(error_output)])
+            else:
+                event.add_return("reply", ["未找到要执行的命令"])
+            
             event.prevent_default()
 
-    # 当收到群消息时触发
-    @on(GroupNormalMessageReceived)
-    def group_normal_message_received(self, event: EventContext, **kwargs):
-        msg = kwargs['text_message']
-        if msg == "hello":  # 如果消息为hello
-
-            # 输出调试信息
-            logging.debug("hello, {}".format(kwargs['sender_id']))
-
-            # 回复消息 "hello, everyone!"
-            event.add_return("reply", ["hello, everyone!"])
-
-            # 阻止该事件默认行为（向接口获取回复）
-            event.prevent_default()
-
-    # 插件卸载时触发
     def __del__(self):
         pass
